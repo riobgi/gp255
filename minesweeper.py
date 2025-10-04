7cd723f01541d78a3fb3fc80ea7d851d99b44c55
chernand7791-oss <chernand7791@oakton.edu>
2025-10-04 15:06:29 -0500

    Mine placement and flagging
    Added random placement of mines and the ability to flag them

--- minesweeper.py
+++ minesweeper.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+import random
 
 
 class Minesweeper:
@@ -13,31 +14,50 @@ class Minesweeper:
         self.mine_positions = set()
         self.numbers = {}
         self.game_over = False
+        self.first_click = True
 
         self.setup_ui()
-        self.default_placer()
+        # self.default_placer()
         self.calculate_numbers()
+        
 
     def setup_ui(self):
         self.root.title("Minesweeper")
         for r in range(self.rows):
             for c in range(self.cols):
-                btn = tk.Button(self.root, width=3, height=1,
-                                command=lambda r=r, c=c: self.click_cell(r, c)
-                                )
+                btn = tk.Button(self.root, width=3, height=1)
                 btn.grid(row=r, column=c)
                 self.buttons[(r, c)] = btn
-
-    # TODO: We need a random or strategic placer, currently placed mines top-left, row by row
+                
+                btn.bind("<Button-1>", lambda event, r=r, c=c: self.left_click(r, c))
+                btn.bind("<Button-3>", lambda event, r=r, c=c: self.right_click(r, c))
+    
+    # this function will be used to ensure that the first click is always safe
+    def safe_first_click(self, safe_r, safe_c):
+        all_positions = [(r, c) for r in range(self.rows) for c in range(self.cols)]
+        
+        # creates 3 x 3 safe area
+        safe_zone = [(r, c)
+                     #range(inclusive, exclusive)
+            for r in range(safe_r - 1, safe_r + 2)
+            for c in range(safe_c - 1, safe_c + 2)
+            # check to make sure it stays within the board
+            if 0 <= r < self.rows and 0 <= c < self.cols]
+        # available positions includes all positions that are not in the safe zone
+        available_positions = [pos for pos in all_positions if pos not in safe_zone]
+        
+        # set the mine positions
+        self.mine_positions = set(random.sample(available_positions, self.mines))
+        # recalculate the numbers
+        self.calculate_numbers()
+        
+        
+    # 🚩 use this emoji to place a flag later on
     def default_placer(self):
-        count = 0
-        for r in range(self.rows):
-            for c in range(self.cols):
-                if count < self.mines:
-                    self.mine_positions.add((r, c))  # change here to randomly place mine
-                    count += 1
-                else:
-                    return
+        # make a list of possible locations
+        all_positions = [(r, c) for r in range(self.rows) for c in range(self.cols)]
+        # take a random sample of the locations to be mines
+        self.mine_positions = set(random.sample(all_positions, self.mines))
 
     def calculate_numbers(self):
         # Calculate adjacent mine counts for each cell
@@ -54,9 +74,14 @@ class Minesweeper:
                                 count += 1
                     self.numbers[(r, c)] = count
 
-    def click_cell(self, r, c):
+    def left_click(self, r, c):
         if self.game_over or self.buttons[(r, c)]['state'] == tk.DISABLED:
             return
+        
+        # the following code bit is to ensure that the first spot you click is safe
+        if self.first_click:
+            self.safe_first_click(r, c)
+            self.first_click = False
 
         btn = self.buttons[(r, c)]
         if (r, c) in self.mine_positions:
@@ -65,6 +90,18 @@ class Minesweeper:
         else:
             self.reveal_cell(r, c)
             # TODO: Haven't implemented win check
+    
+    def right_click(self, r, c):
+        btn = self.buttons[(r,c)]
+        # do nothing if the button is disabled (has been revealed)
+        if btn['state'] == tk.DISABLED:
+            return
+        
+        current_text = btn.cget("text")
+        if current_text == "🚩":
+            btn.config(text="")
+        else:
+            btn.config(text="🚩")
 
     def reveal_cell(self, r, c):
         btn = self.buttons[(r, c)]
